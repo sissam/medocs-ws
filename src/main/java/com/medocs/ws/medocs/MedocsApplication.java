d169cbae4df3f17d5868998e92403026c28edda6
Issam SAIDI <issam.saidi@gmail.com>
2020-10-12 23:51:33 +0200

    first commit

---
+++ src/main/java/com/medocs/ws/medocs/MedocsApplication.java
@@ -0,0 +1,13 @@
+package com.medocs.ws.medocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medocs/ws/medocs/MedocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medocs.ws.medocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
